7a53233f223535e9caae12e925d05c2f30b1bfd2
Александр Мигилев <e-yh@outlook.com>
2022-02-06 13:57:45 +0300

    ok

---
+++ Models/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2Task2.Models
+{
+    class Calc
+    {
+        static public double SqrtCalcul(double a) => Math.Sqrt(a);
+        static public double PowerCalcul(double a, double b) => Math.Pow(a, b);
+    }
+}
